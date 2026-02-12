a8800986f62058d979ae3d1d6fa64fc663f826a6
jccoacore <jccoa.core@gmail.com>
2026-02-12 07:59:37 +0530

    Add Firebase configuration file

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBlmrGDdGBR2VItLa5iNqvy_GyC1Tp-YG4",
+  authDomain: "jcc-block-manager.firebaseapp.com",
+  projectId: "jcc-block-manager",
+  storageBucket: "jcc-block-manager.firebasestorage.app",
+  messagingSenderId: "1013753394181",
+  appId: "1:1013753394181:web:1c0a826908e3345877186c",
+  measurementId: "G-PN4D1DEF5N"
+};
